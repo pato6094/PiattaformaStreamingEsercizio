577aedf85c79e2aeb75fc67c8c0206d864123887
pato6094 <111519569+pato6094@users.noreply.github.com>
2022-11-04 10:36:41 +0100

    Aggiungere i file di progetto.

---
+++ PiattaformaStreaming/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace PiattaformaStreaming
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Application Spotify = new Application("Spotify");
+        }
+    }
+
+}
